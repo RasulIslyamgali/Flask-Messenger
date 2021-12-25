c743743a85bfb639a8a2b30491d7a11a23b4ebd2
rasul_islyamgali <rasul.islyamgali@gmail.com>
2021-12-25 19:58:21 +0600

    add templates and add bootstrap cdn

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 import os
 
 
@@ -7,14 +7,24 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return "Домашняя страница"
+    return render_template("home.html")
 
 
 @app.route("/about")
 def about():
-    return "Страница о нас"
+    return render_template("about.html")
+
+
+@app.route("/user/<string:name>/<int:id_>")
+def user_page(name, id_):
+    data = {
+        "name": name,
+        "id": id_
+    }
+    return render_template("user_page.html", data=data)
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
