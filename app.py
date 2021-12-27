7730531b54293e19205e254a5c7eb690b270dfb9
rasul_islyamgali <rasul.islyamgali@gmail.com>
2021-12-28 01:43:16 +0600

    add PostgreSql database and add User class, and connect register template and DB

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
 import os
+from datetime import datetime
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
 # path = "__main__"
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "PostGresSQL"
+URI = os.getenv("DATABASE_URL").replace(__old="postgres", __new="postgresql")
+app.config["SQLALCHEMY_DATABASE_URI"] = URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id_ = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100))
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    date_of_birth = db.Column(db.DateTime)
+    country = db.Column(db.String(100))
+    email = db.Column(db.String(120), unique=True)
+    date_of_register = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return f"<User username: {self.username} id: {self.id_}>"
+
 
 
 @app.route("/")
 def home():
-    try:
-        url = os.getenv("DATABASE_URL")
-    except:
-        url = "URL не был изъят"
-    data = {
-        "URL": url
-    }
-    return render_template("home.html", data=data)
+    return render_template("home.html")
 
 
 @app.route("/about")
@@ -62,9 +75,25 @@ def friends(username):
     return render_template("friends.html", data=data)
 
 
-@app.route("/register")
+@app.route("/register", methods=["POST", "GET"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        lastname = request.form["lastname"]
+        username = request.form["username"]
+        password = request.form["password"]
+        email = request.form["email"]
+
+        user = User(name=name, lastname=lastname, username=username, password=password, email=email)
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "При добавлении статьи произошла ошибка"
+    else:
+        return render_template("register.html")
 
 
 @app.route("/login")
